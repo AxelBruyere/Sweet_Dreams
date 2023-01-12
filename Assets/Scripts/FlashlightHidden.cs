68c51ca15a07c45cdb9cd860ede240c69e46e025
Axel Bruyere <ax.bru99@gmail.com>
2023-01-12 10:12:51 +0100

    Correction lampe cachée

--- Assets/Scripts/FlashlightHidden.cs
+++ Assets/Scripts/FlashlightHidden.cs
@@ -6,7 +6,7 @@ public class FlashlightHidden : MonoBehaviour
 {
     //bool to indicate if flashlight is on/off
     private bool flashlightActive = false;
-
+    public Camera cameraHidden;
     public GameObject PlayerControllerFPS;
 
     public Animator anim;
@@ -19,19 +19,21 @@ public class FlashlightHidden : MonoBehaviour
 
     //check if the key to turn on/off the flashlight is pressed
     void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.F)){
-            if(flashlightActive == false){
-                //turn on the flashlight
-                transform.gameObject.SetActive(true);
-                flashlightActive = true;
+    {   
+        if (cameraHidden.enabled){
+            if(Input.GetKeyDown(KeyCode.F)){
+                if(flashlightActive == false){
+                    //turn on the flashlight
+                    transform.gameObject.SetActive(true);
+                    flashlightActive = true;
 
+                }
 
-
-            }else{
-                //turn off the flashlight
-                transform.gameObject.SetActive(false);
-                flashlightActive = false;  
+                else{
+                    //turn off the flashlight
+                    transform.gameObject.SetActive(false);
+                    flashlightActive = false;  
+                }
             }
         }
     }
