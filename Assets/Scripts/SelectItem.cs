7916b1f71be548530fcfbc15535627896df7f438
Clémentine Thomas <clemt@LAPTOP-CBUD2E49.(none)>
2023-01-13 15:05:34 +0100

    Italianiiiiiiiii

--- Assets/Scripts/SelectItem.cs
+++ Assets/Scripts/SelectItem.cs
@@ -33,8 +33,8 @@ public class SelectItem : MonoBehaviour
         {
             if (slotName == "Monkey") //&& scene = Attic
             {
-                Debug.Log(plush.transform.GetComponent<MeshRenderer>().enabled);
-                plush.transform.GetComponent<MeshRenderer>().enabled=true;
+                //Debug.Log(plush.transform.GetComponent<MeshRenderer>().enabled);
+                //plush.transform.GetComponent<MeshRenderer>().enabled=true;
                 
                 inventory.Remove(plush.GetComponent<ItemControler>().Item);
                 InventoryManager.haveMonkey = false;
