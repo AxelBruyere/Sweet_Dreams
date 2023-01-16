88c70fd6394e017f766bb9aa3a2537c8758ad448
andresteibelfr <andre.cella-steibel@cpe.fr>
2023-01-16 11:03:37 +0100

    Ajout du monstre dans l´attic

--- Assets/Scripts/TimeEvents.cs
+++ Assets/Scripts/TimeEvents.cs
@@ -36,8 +36,8 @@ public class TimeEvents : MonoBehaviour
 
     private void Update(){
         if(dead && !Screamer.isPlaying){
-            Debug.Log("Changement de scène");
-            //SceneManager.LoadScene(7);
+            //Debug.Log("Changement de scène");
+            SceneManager.LoadScene(7);
         }
     }
 
@@ -68,8 +68,9 @@ public class TimeEvents : MonoBehaviour
                 //////////////////////////////////////////////
                 //////////////////////////////////////////////hidingCamera.GetComponent<LookWithMouse>().enabled = false; //Disables camera movements
                 /////////////////////////////////////////////
-                hidingCamera.transform.eulerAngles = new Vector3(0.0f,0.0f,0.0f);
+                //hidingCamera.transform.eulerAngles = new Vector3(0.0f,0.0f,0.0f);
                 yield return new WaitForSeconds(1.0f); //Waits a few frames
+                Debug.Log("Rallume");
                 flashlightHidden.GetComponent<Light>().intensity = 100.0f; //Makes the light turns back on 
                 Screamer.Play();
                 dead = true;
